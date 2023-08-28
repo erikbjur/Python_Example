57a88c6d1d0d135bf42ce5960b0e83b1ceaa834c
Erik Bjur <erik@bjurengineering.com>
2023-08-27 18:39:12 -0700

    Added code for class email

---
+++ Test.py
@@ -0,0 +1,7 @@
+roster = ['Zachary', 'Eli', 'Riley', 'Adalee', 'Samuel', 'Lucas', 'James', 'Mic', 'Re', 'Ansel']
+
+print("We have " + str(len(roster)) + " students signed up for our programming class")
+for student in roster:
+    print("I'm so excited " + str(student) + " is in my class")
+
+print("You are all going to be amazed at what you will learn this year")
